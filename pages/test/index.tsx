b430a99c5c55b95a75c6b01c4f0542947f8c44be
42KIM <ksahoo524@gmail.com>
2022-02-10 23:49:15 +0900

    test: 컴포넌트 테스트를 위한 임시 test 페이지 생성

---
+++ pages/test/index.tsx
@@ -0,0 +1,11 @@
+import Text from '../../src/components/base/Text';
+
+const TestPage = () => {
+  return (
+    <Text size={'01px'} color={'red'} bold>
+      hello world
+    </Text>
+  );
+};
+
+export default TestPage;

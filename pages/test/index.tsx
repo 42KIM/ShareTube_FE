2d6a7144fe4072cd51cb72c0cda1982fe2de76b8
42KIM <ksahoo524@gmail.com>
2022-02-12 20:10:20 +0900

    test: button 컴포넌트 UI 테스트

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,10 +1,16 @@
 import Text from '../../src/components/base/Text';
+import Button from '../../src/components/base/Button';
 
 const TestPage = () => {
   return (
-    <Text size={'01px'} color={'red'} bold>
-      hello world
-    </Text>
+    <>
+      <Text size={'01px'} color={'red'} bold>
+        hello world
+      </Text>
+      <Button size='lg' color='white' boxShadow>
+        click me
+      </Button>
+    </>
   );
 };
 

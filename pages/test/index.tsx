e76557b95991640b9f2200dd981900dfb1c2bb02
42KIM <ksahoo524@gmail.com>
2022-02-14 23:22:52 +0900

    test: dropdown 컴포넌트 UI 테스트

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,10 +1,31 @@
 import Text from '../../src/components/base/Text';
+import Dropdown from '../../src/components/base/Dropdown';
 
 const TestPage = () => {
+  const dropContent = [
+    {
+      link: '/drop1',
+      text: 'drop1'
+    },
+    {
+      link: '/drop2',
+      text: 'drop2'
+    },
+    {
+      link: '/drop3',
+      text: 'drop3'
+    }
+  ];
+
   return (
-    <Text size={'01px'} color={'red'} bold>
-      hello world
-    </Text>
+    <>
+      <Text size={'01px'} color={'red'} bold>
+        hello world
+      </Text>
+      <Dropdown size={'sm'} color='gray' content={dropContent}>
+        Drop Menu!
+      </Dropdown>
+    </>
   );
 };
 

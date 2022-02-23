c07a4912865926fc9f39d3b20927e6b790906d92
42KIM <ksahoo524@gmail.com>
2022-02-24 02:21:46 +0900

    feat: dashBoard 컴포넌트 Container 분리

---
+++ src/components/domain/Dashboard/Container/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+export { StyledContainer };

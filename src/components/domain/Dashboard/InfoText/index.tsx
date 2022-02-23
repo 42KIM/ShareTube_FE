a566aed054caa732d8bd8bcc5786d33906a9ba24
42KIM <ksahoo524@gmail.com>
2022-02-24 02:38:45 +0900

    feat: 컴포넌트 children 텍스트를 가독성을 고려하여 브라켓으로 감싸기

--- src/components/domain/Dashboard/InfoText/index.tsx
+++ src/components/domain/Dashboard/InfoText/index.tsx
@@ -12,19 +12,19 @@ const InfoText = ({
   let percentile = '0.0';
 
   return isLoggedIn ? (
-    <>
+    <div>
       <Text size={textSize} bold>
         {name}
       </Text>
-      <Text size={textSize}>님은 구독 채널 수</Text>
+      <Text size={textSize}>{'님은 구독 채널 수 '}</Text>
       <Text size={textSize} color={highlightColor} bold>
         {percentile + '%'}
       </Text>
-      <Text size={textSize}>입니다</Text>
-    </>
+      <Text size={textSize}>{'입니다.'}</Text>
+    </div>
   ) : (
     <Text size={textSize} bold>
-      구독자의 랭킹이 궁금하다면 로그인 해주세요!
+      {'구독자의 랭킹이 궁금하다면 로그인 해주세요!'}
     </Text>
   );
 };

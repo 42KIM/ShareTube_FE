a52ccc255fc9a89897244441f75ea0f67afd92c4
42KIM <ksahoo524@gmail.com>
2022-02-24 02:39:09 +0900

    feat: domain/Dashboard 컴포넌트 구현

---
+++ src/components/domain/Dashboard/index.ts
@@ -0,0 +1,6 @@
+import { default as Container } from './Container';
+import { default as InfoBoard } from './InfoBoard';
+import { default as InfoText } from './InfoText';
+import { default as LinkText } from './LinkText';
+
+export default { Container, InfoBoard, InfoText, LinkText };

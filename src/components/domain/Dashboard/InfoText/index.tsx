938015cfc3d4017f111d227191784df0c9fa587b
42KIM <ksahoo524@gmail.com>
2022-03-03 18:19:51 +0900

    style: dashboard.InfoText 컴포넌트 스타일 추가

--- src/components/domain/Dashboard/InfoText/index.tsx
+++ src/components/domain/Dashboard/InfoText/index.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
 import { Text } from '@/base';
+import { StyledContainer } from './styles';
 import type { InfoTextProps } from './types';
 
 const InfoText = ({
@@ -7,25 +8,29 @@ const InfoText = ({
   highlightColor
 }: InfoTextProps): ReactElement => {
   // from store
-  let isLoggedIn = true;
+  let isLoggedIn = false;
   let name = 'User';
   let percentile = '0.0';
 
-  return isLoggedIn ? (
-    <div>
-      <Text font={textFont} bold>
-        {name}
-      </Text>
-      <Text font={textFont}>{'님은 구독 채널 수 '}</Text>
-      <Text font={textFont} color={highlightColor} bold>
-        {percentile + '%'}
-      </Text>
-      <Text font={textFont}>{'입니다.'}</Text>
-    </div>
-  ) : (
-    <Text font={textFont} bold>
-      {'구독자의 랭킹이 궁금하다면 로그인 해주세요!'}
-    </Text>
+  return (
+    <StyledContainer>
+      {isLoggedIn ? (
+        <>
+          <Text font={textFont} bold>
+            {name}
+          </Text>
+          <Text font={textFont}>{'님은 구독 채널 수 '}</Text>
+          <Text font={textFont} color={highlightColor} bold>
+            {percentile + '%'}
+          </Text>
+          <Text font={textFont}>{'입니다.'}</Text>
+        </>
+      ) : (
+        <Text font={textFont} bold>
+          {'당신의 YouTube 구독 랭킹이 궁금하다면 로그인 해보세요!'}
+        </Text>
+      )}
+    </StyledContainer>
   );
 };
 
---
+++ src/components/domain/Dashboard/InfoText/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30%;
+`;
+
+export { StyledContainer };

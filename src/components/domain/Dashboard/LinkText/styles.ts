36aa3164be3ad0bacc99220cfa76ea8c4f8dcd6f
42KIM <ksahoo524@gmail.com>
2022-03-03 18:19:22 +0900

    style: dashboard.Linktext 컴포넌트 스타일 추가

--- src/components/domain/Dashboard/LinkText/styles.ts
+++ src/components/domain/Dashboard/LinkText/styles.ts
@@ -1,8 +1,14 @@
+import { COLOR } from '@/constants';
 import styled from 'styled-components';
 
 // temp
 const StyledLinkText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
+  height: 44px;
+  background-color: ${COLOR.coral};
 `;
 
 export { StyledLinkText };

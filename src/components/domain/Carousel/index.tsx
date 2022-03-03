5e33e08f8ac6e208977189839cc8916d69e0dd12
42KIM <ksahoo524@gmail.com>
2022-03-03 18:22:14 +0900

    feat: carousel 컴포넌트 로그인 분기 처리

--- src/components/domain/Carousel/index.tsx
+++ src/components/domain/Carousel/index.tsx
@@ -1,22 +1,28 @@
 import { ReactElement, useEffect, useRef, Children } from 'react';
-import { StyledContainer } from './styles';
+import { StyledContainer, StyledCardContainer } from './styles';
+import { Text } from '@/base';
 import { ChannelCard } from '@/domain';
 import Link from 'next/link';
+import { FONT } from '@/constants';
 
 // DUMMY
 const channel = {
   url: 'http://www.youtube.com',
   thumbnail:
     'https://yt3.ggpht.com/ytc/AKedOLTi6w4E6985-QdVBbovBSsnCeTETyj0WomjM5IY8Q=s900-c-k-c0x00ffffff-no-rj',
-  title: '침착맨',
+  title: '킹받네KG받드라슈어쩔티비저쩔티비',
   subTitle: '11.1'
 };
 
 const Carousel = (): ReactElement => {
   // from store.
+  let isLoggedIn = false;
+  let username = 'User';
   // top 10 channels
   // IChannel 타입 필요
   let topChannels = [
+    channel,
+    channel,
     channel,
     channel,
     channel,
@@ -49,23 +55,30 @@ const Carousel = (): ReactElement => {
   });
 
   return (
-    <StyledContainer ref={wheelRef}>
-      {Children.toArray(
-        topChannels.map((channel) => (
-          <ChannelCard
-            url={channel.url}
-            size={'150px'}
-            thumbnail={channel.thumbnail}
-            title={channel.title}
-            subTitle={channel.subTitle}
-          />
-        ))
+    <StyledContainer>
+      {isLoggedIn ? (
+        <Text font={FONT.h3_bolder}>{`${username}님이 구독 중인 채널 >`}</Text>
+      ) : (
+        <Text font={FONT.h3_bolder}>{'구독 중인 채널 목록 >'}</Text>
       )}
-      <Link href={'/list'}>
-        <a>
-          <ChannelCard size={'150px'} />
-        </a>
-      </Link>
+      <StyledCardContainer ref={wheelRef}>
+        {Children.toArray(
+          topChannels.map((channel) => (
+            <ChannelCard
+              url={channel.url}
+              size={'150px'}
+              thumbnail={channel.thumbnail}
+              title={channel.title}
+              subTitle={channel.subTitle}
+            />
+          ))
+        )}
+        <Link href={'/list'}>
+          <a>
+            <ChannelCard size={'150px'} />
+          </a>
+        </Link>
+      </StyledCardContainer>
     </StyledContainer>
   );
 };

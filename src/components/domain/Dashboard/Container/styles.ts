854560865cec6d90a4dadee1ae3656b57917186f
42KIM <ksahoo524@gmail.com>
2022-03-03 18:26:48 +0900

    style: dashboard 컴포넌트 스타일 추가

--- src/components/domain/Dashboard/Container/styles.ts
+++ src/components/domain/Dashboard/Container/styles.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import { COLOR } from '@/constants';
 
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+  height: 280px;
   overflow: hidden;
+  background-color: ${COLOR.whiteGray};
+  border-radius: 20px;
 `;
 
 export { StyledContainer };

d07f0042d577c3bb64b8d78b4b664cecb801b2cc
42KIM <ksahoo524@gmail.com>
2022-03-07 20:20:55 +0900

    feat: domain/List.CreateButton 컴포넌트 구현

---
+++ src/components/domain/List/CreateButton/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledTextWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: center;
+`;
+
+export { StyledTextWrapper };

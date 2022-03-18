e33543e8f3ee36e8dacdccdad050f7ca0fa6b5a8
42KIM <ksahoo524@gmail.com>
2022-03-18 14:56:41 +0900

    chore: 로그인 시 유저 데이터 받아오는 함수명 변경

--- src/apis/auth/index.tsx
+++ src/apis/auth/index.tsx
@@ -2,8 +2,9 @@ import { request, authRequest } from '../axios';
 import { ILogInPayload } from '@/types';
 
 const authApi = {
-  getLoginData: (id: string): Promise<ILogInPayload> =>
-    request.get(`/auth/result/${id}`)
+  getInitialData: (id: string): Promise<ILogInPayload> =>
+    request.get(`/auth/result/${id}`),
+  logout: (): Promise<string> => authRequest.get('/auth/logout')
 };
 
 export default authApi;

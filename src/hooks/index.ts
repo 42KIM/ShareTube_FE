8cd14b9f6bfe0c87f171da17d08872ed596e5b23
42KIM <ksahoo524@gmail.com>
2022-03-21 22:59:43 +0900

    feat: modal을 위한 useClickAway hook 구현

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useAppSelector } from './useAppSelector';
+export { default as useClickAway } from './useClickAway';
 export { default as useLocalStorage } from './useLocalStorage';

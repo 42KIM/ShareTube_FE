50cadb572317ee30db36aaf53d2d1aa514ce69e6
42KIM <ksahoo524@gmail.com>
2022-03-24 23:41:29 +0900

    feat: auth, list 관련 api 호출 함수 일부 추가 (미구현)

---
+++ src/apis/list/index.ts
@@ -0,0 +1,9 @@
+import { request, authRequest } from '../axios';
+
+const listApi = {
+  createMyList: () => {},
+  deleteMyList: () => {},
+  deleteSharedList: () => {}
+};
+
+export default listApi;

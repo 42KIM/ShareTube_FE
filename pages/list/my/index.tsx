ac6a55c08c7c43c262310fd6311936f9e5a3e13a
42KIM <ksahoo524@gmail.com>
2022-03-24 23:38:05 +0900

    feat: list/my 페이지에 리덕스 일부 적용

--- pages/list/my/index.tsx
+++ pages/list/my/index.tsx
@@ -1,82 +1,34 @@
-import { ReactElement, useEffect, useState, Children } from 'react';
-import { Icon, Text } from '@/base';
+import { ReactElement, useEffect, Children } from 'react';
 import { List } from '@/domain';
-import { FONT, ICON_SIZE } from '@/constants';
-
-// from store
-let myListItems = [
-  {
-    id: 11,
-    thumbnail:
-      'https://yt3.ggpht.com/ytc/AKedOLTi6w4E6985-QdVBbovBSsnCeTETyj0WomjM5IY8Q=s900-c-k-c0x00ffffff-no-rj',
-    title: '우울할 때 보는 리스트1',
-    views: 123
-  },
-  {
-    id: 22,
-    thumbnail:
-      'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg',
-    title: '우울할 때 보는 리스트2',
-    views: 345
-  }
-];
-let sharedListItems = [
-  {
-    id: 33,
-    thumbnail:
-      'https://yt3.ggpht.com/ytc/AKedOLTi6w4E6985-QdVBbovBSsnCeTETyj0WomjM5IY8Q=s900-c-k-c0x00ffffff-no-rj',
-    title: '친구가 공유해준 거!',
-    views: 42
-  }
-];
-let mylistTotal = 2;
-let savedSharedListTotal = 1;
+import { useAppSelector } from '@/hooks';
+import { useDispatch } from 'react-redux';
+import { getMyLists } from '@/store';
+import { listApi } from '@/apis';
 
 const MylistPage = (): ReactElement => {
-  const [tab, setTab] = useState<{
-    firstTabSelected: boolean;
-    // list interface 필요
-    listItems: any[];
-  }>({ firstTabSelected: true, listItems: [] });
+  const { totalCreatedListCount, createdList } = useAppSelector(
+    (state) => state.list.my
+  );
+  const dispatch = useDispatch();
 
-  // 최초에는 마이리스트 렌더
   useEffect(() => {
-    setTab((initialState) => {
-      return {
-        ...initialState,
-        listItems: myListItems
-      };
-    });
+    dispatch(getMyLists());
   }, []);
 
-  const handleTab = () => {
-    if (tab.firstTabSelected) {
-      setTab({
-        firstTabSelected: false,
-        listItems: sharedListItems
-      });
-    } else {
-      setTab({
-        firstTabSelected: true,
-        listItems: myListItems
-      });
-    }
+  const handleDelete = () => {
+    console.log('mylist delete!');
+    listApi.deleteMyList();
+    // alert창
   };
-
-  // mylist 삭제하기 api 호출
-  const deleteList = () => console.log('delete!');
 
   return (
     <>
-      <List.Tab tabState={tab} tabHandler={handleTab} />
-      <List.InfoBox
-        tabState={tab}
-        listInfo={tab.firstTabSelected ? mylistTotal : savedSharedListTotal}
-      />
+      <List.Tab />
+      <List.InfoBox path={'/list/my'} listInfo={totalCreatedListCount} />
       <List.InnerContainer>
         <List.CreateButton />
         {Children.toArray(
-          tab.listItems.map(({ id, thumbnail, title, views }) => (
+          createdList.map(({ id, thumbnail, title, views }) => (
             <List.ListItem
               id={id}
               thumbnail={thumbnail}
@@ -85,10 +37,7 @@ const MylistPage = (): ReactElement => {
             />
           ))
         )}
-        <List.FloatButton handler={deleteList}>
-          <Icon icon={'listDelete'} size={ICON_SIZE.md} />
-          <Text font={FONT.h1_bold}>리스트 삭제하기</Text>
-        </List.FloatButton>
+        <List.FloatButton handler={handleDelete} />
       </List.InnerContainer>
     </>
   );

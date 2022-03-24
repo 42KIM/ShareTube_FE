74b196e9f39e860674b83025c066e2cc6d62dc52
42KIM <ksahoo524@gmail.com>
2022-03-24 23:44:25 +0900

    feat: createAsyncThunk를 사용하여 구독 목록을 fetch하는 thunk 구현

--- src/store/subscription/index.ts
+++ src/store/subscription/index.ts
@@ -1,27 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { HYDRATE } from 'next-redux-wrapper';
-import type { IChannel } from '@/types';
+import type { IChannel, ISubsFull } from '@/types';
+import { subscriptionApi } from '@/apis';
 
 const initialState = {
   totalSubscriptions: 0,
   channelDetails: [] as IChannel[]
 };
 
+const getSubscriptions = createAsyncThunk(
+  'subscription/getSubscriptions',
+  async () => {
+    const response = await subscriptionApi.getFullSubscriptions();
+    return response;
+  }
+);
+
 const slice = createSlice({
   name: 'subscription',
   initialState,
   reducers: {
     // Hydrate action?
-    [HYDRATE]: () => {},
-    getSubscriptions: (state) => {
-      // 비동기로 서버로부터 구독 목록 리스트를 받아서 업데이트 한다.
-      return {
-        ...state
-      };
-    }
+    [HYDRATE]: () => {}
+  },
+  extraReducers: {
+    [getSubscriptions.fulfilled.type]: (
+      state,
+      action: PayloadAction<ISubsFull>
+    ) => ({
+      ...state,
+      totalSubscriptions: action.payload.totalSubscriptions,
+      channelDetails: action.payload.channelDetails
+    })
+    // [getSubscriptions.rejected.type]: () => {}
   }
 });
 
 export const subscriptionReducer = slice.reducer;
 
-export const { getSubscriptions } = slice.actions;
+export { getSubscriptions };
